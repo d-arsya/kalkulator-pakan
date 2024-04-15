5ef90a23ce771ff0c83112cbb06b45f5a0e89565
Kamaluddin Arsyad Fadllillah <102853169+d-arsya@users.noreply.github.com>
2024-04-16 04:24:34 +0700

    Update table class

--- src/components/KomposisiBahan.jsx
+++ src/components/KomposisiBahan.jsx
@@ -81,7 +81,7 @@ export default function KomposisiBahan({ bahanUse }) {
       </h2>
 
       <h5 className="my-3 text-center">Total Bahan</h5>
-      <table>
+      <table className="table">
         <thead>
           <tr className="text-center">
             <th style={{width:"10vw"}}>BK</th>
