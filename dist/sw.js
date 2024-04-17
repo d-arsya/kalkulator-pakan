80f493cabe59c917a481be0cc19613dadc66dd62
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-04-17 22:07:42 +0700

    pwa

---
+++ dist/sw.js
@@ -0,0 +1,34 @@
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      "/",
+      "/index.html",
+      "/assets/index-qE4NpTUm.js",
+      "/assets/index-CXlQ_1Pk.css",
+    ]),
+  );
+});
+const putInCache = async (request, response) => {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+};
+
+const cacheFirst = async (request) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+  const responseFromNetwork = await fetch(request);
+  putInCache(request, responseFromNetwork.clone());
+  return responseFromNetwork;
+};
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(cacheFirst(event.request));
+});
+
